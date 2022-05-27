9da8929b57003f9022a38f4bdeb5e17ebe4bcb3b
hero1313 <nikoloz.kaulashvili.1@btu.edu.ge>
2022-05-27 12:19:07 -0800

    nikas-front

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(".hidden-navigation").css("display", "none");
+$( ".hide-nav" ).click(function() {
+    $(".hidden-navigation").css("display", "block");
+});
+$( ".hidden-navigation" ).click(function() {
+    $(".hidden-navigation").css("display", "none");
+});
